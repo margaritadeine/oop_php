41da34df81b5bec2383a361664505e354cc05710
margarita.deine <mademoselle@gmail.com>
2018-12-11 08:42:39 +0200

    Katsetasime php faili tööd  serverile üleslaadimisega

---
+++ katse.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: margarita.deinekina
+ * Date: 06.12.2018
+ * Time: 11:08
+ */
+
+
+echo '
+    <!doctype html>
+    <html>
+        <head>
+                <title></title>
+        </head>
+        <body>
+            <h1>PHP abil loodud HMTL</h1>
+        </body>
+     </html>
+ ';
+?>
