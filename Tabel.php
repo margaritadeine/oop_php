67cc4e29db81d306e7bad3445ee2f36d248f3af1
margarita.deine <mademoselle@gmail.com>
2018-12-13 10:43:16 +0200

    Klassid ja OOP - Tabel klassi kirjeldus on loodud

---
+++ Tabel.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: margarita.deinekina
+ * Date: 13.12.2018
+ * Time: 10:28
+ */
+
+class Tabel
+{
+    //antud omadused
+    var $tabel = array();
+    //klassi meetodid
+
+    /**
+     * @param $rida -tegemist on massiiv rea elementidega
+     */
+    function  lisaRida($rida){
+        $this->tabel[] = $rida;
+    }
+
+    /**
+     * vaata tabel massiv foreach abil ridade kaupa
+     * ja väljasta iga rea sees olev element tühikuga erladades
+     * read eralda reavahetusega
+     */
+    function naitaTabel(){
+        foreach ($this->tabel as $rida){
+            foreach ($rida as $element){
+                echo $element.'<&nbsp';
+            }
+            echo '<br>';
+        }
+    }
+
+}
