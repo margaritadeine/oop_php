effe2afaeb68529dae5e837f6accef5f9624dae8
margarita.deine <mademoselle@gmail.com>
2018-12-18 11:05:56 +0200

    Vormid - üheleheline vorm

--- katse.php
+++ katse.php
@@ -7,12 +7,26 @@
  */
 
 
-//lehe sisu väljastamine
-echo '<!doctype html><html><head>
-<title>Vormid</title>
-<link rel="stylesheet" type="text/css" href="katsestyle.css">
-</head><body>';
-//kutsume funktsiooni tööle
-
-echo '</body></html>';
+function vorm(){
+    echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">
+        <div>
+            <label>kasutajanimi :</label>
+            <input type="text" name="username">
+        </div>
+        <input type="submit" value="Saada!">
+    </form>';
+}
+    if(empty($_POST)){
+        vorm();
+    } else {
+        foreach ($_POST as $element) {
+            if (empty($element)) {
+                echo 'Andmed tuleb sisestada ka!<br>';
+                echo '<a href="' . $_SERVER['PHP_SELF'] . '">Proovi uuesti!</a>';
+                exit;
+            }
+        }
+        echo 'kasutajanimi = ' . $_POST['username'] . '<br>';
+        echo 'Tere tulemast ' . $_POST['username'] . '!<br>';
+    }
 ?>
