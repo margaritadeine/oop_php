5e0c5b5ee1e767b3f0420c5f825939430008eca3
margarita.deine <mademoselle@gmail.com>
2018-12-18 10:43:49 +0200

    Vormid - vormitöötluse lehe turvamine

--- vormiTootlus.php
+++ vormiTootlus.php
@@ -5,6 +5,16 @@
  * Date: 18.12.2018
  * Time: 10:27
  */
-echo '<pre>';
+/*echo '<pre>';
 print_r($_POST);
-echo '<pre>';
+echo '</pre>';
+echo '</pre>';*/
+
+
+if(empty($_POST)){
+    echo '<a href="vorm.php">Logi sisse!</a>';
+} else {
+    echo 'kasutajanimi = ' . $_POST['username'] . '<br>';
+    echo 'parool = ' . $_POST['passwd'] . '<br>';
+    echo 'Tere tulemast ' . $_POST['username'] . '!<br>';
+}
