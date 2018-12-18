d411286fd9d8c552096ba286b1203b76eb4fb399
margarita.deine <mademoselle@gmail.com>
2018-12-18 10:51:58 +0200

    Vormid - vormitöötluse turvamine tühjate väljade eest

--- vormiTootlus.php
+++ vormiTootlus.php
@@ -10,10 +10,21 @@ print_r($_POST);
 echo '</pre>';
 echo '</pre>';*/
 
+echo '<pre>';
+print_r($_POST);
 
 if(empty($_POST)){
-    echo '<a href="vorm.php">Logi sisse!</a>';
+    //echo '<a href="vorm.php">Logi sisse!</a>';
+    //    echo '<a href="vorm.php">Logi sisse!</a>';
+    header('Location: vorm.php');
 } else {
+    foreach ($_POST as $element){
+        if(empty($element)){
+            echo 'Andmed tuleb sisestada ka!<br>';
+            echo '<a href="vorm.php">Proovi uuesti!</a>';
+            exit;
+        }
+    }
     echo 'kasutajanimi = ' . $_POST['username'] . '<br>';
     echo 'parool = ' . $_POST['passwd'] . '<br>';
     echo 'Tere tulemast ' . $_POST['username'] . '!<br>';
